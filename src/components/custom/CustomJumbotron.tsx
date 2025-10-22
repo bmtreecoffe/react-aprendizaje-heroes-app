b1abd3482590e9a183cad7536cd8c6bbdc101e99
Jaime Fco Borre Pascuales <bmtreecoffe@gmail.com>
2025-10-21 20:22:49 -0500

    Se modifican archivos para cabecera

---
+++ src/components/custom/CustomJumbotron.tsx
@@ -0,0 +1,19 @@
+import type { FC } from "react";
+
+interface Props {
+    title: string;
+    description: string;
+}
+
+export const CustomJumbotron: FC<Props> = ({title, description}) => {
+    return (
+        <div className="text-center mb-8">
+            <h1 className="text-5xl font-bold bg-linear-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent mb-4">
+                {title}
+            </h1>
+            {
+                description && ( <p className="text-gray-600 text-lg">description</p>)
+            }           
+        </div>
+    )
+}
